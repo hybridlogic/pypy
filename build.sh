bace3ce98518674dc5086ecf87db62f7617a7759
Hybrid Cluster <hybrid@9325b5b411466399.dark.hybrid-cloud.com>
2013-02-12 11:09:54 +0000

    first commit

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ "x$1" -eq "x" ]; then
+    echo "Usage: build.sh <version-id>"
+    exit
+fi
+cd /home/hybrid/pypy/pypy/goal
+pypy ../../rpython/bin/rpython -Ojit --lldebug targetpypystandalone --objspace-lonepycfiles
+cd /home/hybrid/pypy/pypy/tool/release
+pypy package.py --nostrip ../../../ pypy-$1-freebsd64
